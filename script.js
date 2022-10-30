68b20e46ecf795aff37d870e35d0fca68dd262ba
jaywon joo <jaywonjoo@gmail.com>
2022-10-30 12:30:34 -0700

    Advanced todo code
    Was too advanced for me (or I'm burning out). Needed tutorial.

--- script.js
+++ script.js
@@ -1,33 +1,73 @@
-const form = document.getElementById("new-todo-form");
-const input = document.querySelector("#todo-input");
-// const list = document.querySelector("#list");
-//const div = document.querySelector("div");
+const form = document.querySelector("#new-todo-form");
+const todoInput = document.querySelector("#todo-input");
+const list = document.querySelector("#list");
+const template = document.querySelector("#list-item-template");
+const LOCAL_STORAGE_PREFIX = "ADVANCED_TODO_LIST";
+const TODOS_STORAGE_KEY = `${LOCAL_STORAGE_PREFIX}-todos`;
+let todos = loadTodos();
+todos.forEach(renderTodo);
+
+list.addEventListener("change", (e) => {
+  if (!e.target.matches(`[data-list-item-checkbox]`)) return;
+
+  // Get the todo that is clicked on
+  const parent = e.target.closest(".list-item");
+  const todoId = parent.dataset.todoId;
+  const todo = todos.find((t) => t.id === todoId);
+  // toggle the complete property to be equal to the checkbox value
+  todo.complete = e.target.checked;
+  // save our updated todo
+  saveTodos();
+});
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-
-  // create new li
-  const newEmptyListItem = document.createElement("li");
-
-  // grab value inputted to input
-  let todoItem = input.value;
-
-  // put a checkbox on the list item
-  // put a delete button on the list item
-
-  // put inputted value into <li>
-  newEmptyListItem.innerHTML = todoItem;
-
-  // append <li> to list
-  let newListItem = list.appendChild(newEmptyListItem);
-
-  // add class list to created li
-  newListItem.classList.add("list-item-label");
-
-  const createdItem = document.getElementsByClassName("list-item-label");
-  newListItem.addEventListener("click", () => {
-    // console.log("blah");
-    newListItem.remove();
-    // console.log(div.innerHTML);
-  });
+  // prevent empty string from being input
+  const todoName = todoInput.value;
+  if (todoInput.value === "") return;
+  const newTodo = {
+    name: todoName,
+    complete: false,
+    id: new Date().valueOf().toString(),
+  };
+  todos.push(newTodo);
+  renderTodo(newTodo);
+  saveTodos();
+  //   todoInput.value = "";
 });
+
+list.addEventListener("click", (e) => {
+  if (!e.target.matches(`[data-button-delete]`)) return;
+
+  const parent = e.target.closest(".list-item");
+  const todoId = parent.dataset.todoId;
+
+  // remove the todo from the screen
+  parent.remove();
+  // remove the todo from the list
+  todos = todos.filter((todo) => todo.id !== todoId);
+  // save the new todos
+  saveTodos();
+});
+
+function renderTodo(todo) {
+  const templateClone = template.content.cloneNode(true);
+  const listItem = templateClone.querySelector(`.list-item`);
+  listItem.dataset.todoId = todo.id;
+  const textElement = templateClone.querySelector(`[data-list-item-text]`);
+  textElement.innerText = todo.name;
+  const checkbox = templateClone.querySelector(`[data-list-item-checkbox]`);
+  checkbox.checked = todo.complete;
+  list.appendChild(templateClone);
+}
+
+// load Todos
+function loadTodos() {
+  const todosString = localStorage.getItem(TODOS_STORAGE_KEY);
+  return JSON.parse(todosString) || [];
+}
+
+// save Todos
+function saveTodos() {
+  localStorage.setItem(TODOS_STORAGE_KEY, JSON.stringify(todos));
+}
