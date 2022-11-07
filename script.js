f8685e09c10743027b70257aa049fa641b2af9af
jaywonjoo <jaywonjoo@gmail.com>
2022-11-06 17:15:59 -0800

    Hound: Make button open modal
    Modal lets you specify name of the project and pastes it on the card!

--- script.js
+++ script.js
@@ -1,11 +1,57 @@
 // const projectSection = document.querySelector("#project-section");
+
 const button = document.getElementById("button");
 const projectContainer = document.querySelector("#project-container");
 const newProject = document.createElement("div");
 
+// button.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   projectContainer.appendChild(newProject);
+//   newProject.innerText = "blah";
+//   newProject.classList.add("project-card");
+// });
+
+
+
+const modal = document.querySelector("#modal")
+const openModalButton = document.querySelector("#open-modal-btn")
+const closeModalButton = document.querySelector("#close-modal-btn")
+const overlay = document.querySelector("#overlay")
+
 button.addEventListener("click", (e) => {
-  e.preventDefault();
+    e.preventDefault();
+
+    openModal()
+})
+
+
+// click on "New Project" to open modal
+function openModal() {
+    modal.classList.add("open")
+    overlay.classList.add("open")
+}
+
+// user inputs project name
+// user presses "close modal" button
+// JS takes the input and sets it as the inner text of the created div
+
+closeModalButton.addEventListener("click", createProject)
+
+function createProject() {
+  const input = document.querySelector("#item-input");
+  
   projectContainer.appendChild(newProject);
-  newProject.innerText = "blah";
+  newProject.innerText = input.value;
   newProject.classList.add("project-card");
-});
+
+  closeModal()
+}
+
+
+// click on overlay to cancel operation
+overlay.addEventListener("click", closeModal)
+
+function closeModal() {
+  modal.classList.remove("open")
+  overlay.classList.remove("open")
+}
