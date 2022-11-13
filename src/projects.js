8434bf7885e3151f440e63430b5e8c84875b366d
jaywonjoo <jaywonjoo@gmail.com>
2022-11-13 00:44:47 -0800

    Add ticket creation and deletion feature (buggy)
    page doesn't refresh when ticket is deleted??

--- src/projects.js
+++ src/projects.js
@@ -22,7 +22,7 @@ initializeApp(firebaseConfig)
 const db = getFirestore()
 
 // collection ref
-const colRef = collection(db, 'projects', 'ShbJQO2nmY9enT8qY0SW', 'tickets')
+const colRef = collection(db, 'projects', 'FbLa2ligAuIL1PdyolJH', 'tickets')
 
 
 
@@ -45,9 +45,10 @@ const colRef = collection(db, 'projects', 'ShbJQO2nmY9enT8qY0SW', 'tickets')
                 TicketAuthor.innerText = tickets[0].author;
                 ticketContainer.appendChild(TicketAuthor)
             }
-
         })
         console.log(tickets)
+
+
     })
 // 2. append tickets to tickets list
     // a. need constant for ticket <ul>
@@ -58,11 +59,68 @@ const colRef = collection(db, 'projects', 'ShbJQO2nmY9enT8qY0SW', 'tickets')
     const TicketDescription = document.createElement("li");
     const TicketAuthor = document.createElement("li");
 
-
-
     // c. need function that does (2)
         // function was place inside the snapshot!
 
 
+// 3. create ticket form
+const newTicketButton = document.querySelector("#newTicketButton")
+newTicketButton.addEventListener('click', () => {
+    // click on button to open "new ticket" form
+    // click on button to open modal
+    openModal()
+})
 
-// create ticket
+    // click on "New Project" to open modal
+    function openModal() {
+        modal.classList.add("open")
+        overlay.classList.add("open")
+    }
+
+    // click on overlay to cancel operation
+    overlay.addEventListener("click", closeModal)
+    function closeModal() {
+        modal.classList.remove("open")
+        overlay.classList.remove("open")
+    }
+
+    // linking firestore project collection
+    const projectRef = collection(db, 'projects')
+    // submit form after clicking
+    const createTicketForm = document.querySelector('.create-ticket-btn')
+    createTicketForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+        
+        addDoc(colRef, {
+            author: createTicketForm.author.value,
+            description: createTicketForm.description.value,
+            title: createTicketForm.title.value,
+        })
+        .then(() => {
+            createTicketForm.reset()
+        })
+
+        // createTicket()
+        closeModal();
+    })    
+
+
+// 4. Delete Ticket Form
+const deleteTicketForm = document.querySelector('.delete-ticket-form')
+deleteTicketForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const docRef = doc(db, 'projects', 'FbLa2ligAuIL1PdyolJH', 'tickets', deleteTicketForm.id.value)
+    deleteDoc(docRef)
+        .then(() => {
+            deleteTicketForm.reset()
+        })
+})
+
+// ***************************************************************************************************
+
+
+
+
+// 4. display selected ticket info
+// 4.1 populate selected ticket info when clicked
