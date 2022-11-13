099927a0d579cf88bed978046760eac84383701a
jaywonjoo <jaywonjoo@gmail.com>
2022-11-12 19:10:05 -0800

    Display ticket info on project page
    WOO WOO

--- src/projects.js
+++ src/projects.js
@@ -1,1 +1,68 @@
+import { initializeApp } from 'firebase/app';
+import {
+    getFirestore, collection, onSnapshot,
+    addDoc, deleteDoc, doc,
+} from 'firebase/firestore';
 import "./projects.css";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAgs-sGBJrnqvlOBqMbZr_E1hWYJoofA2c",
+    authDomain: "hound-e43f0.firebaseapp.com",
+    projectId: "hound-e43f0",
+    storageBucket: "hound-e43f0.appspot.com",
+    messagingSenderId: "361705338046",
+    appId: "1:361705338046:web:f04df4040689f429aa9aef"
+  };
+
+
+// init firebase app
+initializeApp(firebaseConfig)
+
+// init services
+const db = getFirestore()
+
+// collection ref
+const colRef = collection(db, 'projects', 'ShbJQO2nmY9enT8qY0SW', 'tickets')
+
+
+
+
+// display ticket info in ticket list
+// 1. link tickets to page
+    onSnapshot(colRef, (snapshot) => {
+        let tickets = []
+        snapshot.docs.forEach((doc) => {
+            tickets.push({ ...doc.data(), id: doc.id })
+            showTicket()
+
+            function showTicket() {
+                TicketTitle.innerText = tickets[0].title;
+                ticketContainer.appendChild(TicketTitle)
+
+                TicketDescription.innerText = tickets[0].description;
+                ticketContainer.appendChild(TicketDescription)
+
+                TicketAuthor.innerText = tickets[0].author;
+                ticketContainer.appendChild(TicketAuthor)
+            }
+
+        })
+        console.log(tickets)
+    })
+// 2. append tickets to tickets list
+    // a. need constant for ticket <ul>
+    const ticketContainer = document.querySelector("#tickets")
+
+    // b. need constant that creates an <li> we can append to the <ul>
+    const TicketTitle = document.createElement("li");
+    const TicketDescription = document.createElement("li");
+    const TicketAuthor = document.createElement("li");
+
+
+
+    // c. need function that does (2)
+        // function was place inside the snapshot!
+
+
+
+// create ticket
