0b8726bdd0b95131d14adf86efbf5bb9e2659548
jaywon joo <jaywonjoo@gmail.com>
2022-11-14 13:23:50 -0800

    added in homepage, signin, and signup pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,25 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-    module: {
-        rules: [
-            {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-            },
-        ],
-    },
-  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  mode: "development",
   entry: {
-    index: './src/index.js',
-    projects: './src/projects.js'
+    index: "./src/index.js",
+    signin: "./src/signin.js",
+    signup: "./src/signup.js",
+    dashboard: "./src/dashboard.js",
+    projects: "./src/projects.js",
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
   },
-  watch: true
-}
+  watch: true,
+};
