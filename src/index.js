e2d3ed40349fc649ed4f2a0c823eb5978df7c0e4
jaywon joo <jaywonjoo@gmail.com>
2022-11-28 17:41:24 -0800

    Style homepage

--- src/index.js
+++ src/index.js
@@ -3,13 +3,29 @@ import "./index.css";
 // ****************************************************************************************** //
 
 // button to take users to the signin page
-const loginButton = document.querySelector("#loginButton");
+const loginButton = document.querySelector(".nav-login");
 loginButton.addEventListener("click", () => {  
   window.location.href = "signin.html";
 });
 
 // button to take users to the signup page
-const signUpButton = document.querySelector("#signUpButton");
+const signUpButton = document.querySelector(".nav-register-bubble");
 signUpButton.addEventListener("click", () => {
   window.location.href = "signup.html";
 });
+
+
+
+
+  const toggleButton = document.getElementsByClassName('toggle-button')[0]
+  const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+  const navbarDropdown = document.getElementsByClassName('nav-dropdown')[0]
+
+  toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+  })
+
+
+
+
+
