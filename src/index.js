e345f5fea549e7af079de4137103d8845e82f683
jaywon joo <jaywonjoo@gmail.com>
2022-12-18 13:14:00 -0800

    Clean up Index.html/.css/.js
    index.html:
    1. Cleaned up indents
    2. Deleted comments
    3. Refactored hero section to flex better
    4. Rename variables for better readability
    5. Add in semantic html
    
    index.css:
    1. Organized styles into proper sections
    2. Deleted comments
    
    index.js:
    1. Organize file structure

--- src/index.js
+++ src/index.js
@@ -1,29 +1,32 @@
 import "./index.css";
 
-// ****************************************************************************************** //
-
-// button to take users to the signin page
 const loginButton = document.querySelector(".nav-login");
-loginButton.addEventListener("click", () => {  
-  window.location.href = "signin.html";
-});
-
-// button to take users to the signup page
 const signUpButton = document.querySelector(".nav-register-bubble");
-signUpButton.addEventListener("click", () => {
-  window.location.href = "signup.html";
-});
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const navbarDropdown = document.getElementsByClassName('nav-dropdown')[0]
 
+loginRedirectButton()
+signUpRedirectButton()
+toggleHamburgerMenu()
 
+function loginRedirectButton() {
+  loginButton.addEventListener("click", () => {  
+    window.location.href = "signin.html";
+  });
+}
 
+function signUpRedirectButton() {
+  signUpButton.addEventListener("click", () => {
+    window.location.href = "signup.html";
+  });
+}
 
-  const toggleButton = document.getElementsByClassName('toggle-button')[0]
-  const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-  const navbarDropdown = document.getElementsByClassName('nav-dropdown')[0]
-
+function toggleHamburgerMenu() {
   toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-  })
+    navbarLinks.classList.toggle('active')
+    })  
+}
 
 
 
