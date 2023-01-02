971c4a327b923dc0c03edce8368862f950a1b85d
jaywon joo <jaywonjoo@gmail.com>
2023-01-01 20:01:40 -0800

    Style homepage with bootstrap elements
    1. Add feature logos
    2. Add custom text to features to make it more official
    3. Refine padding to make it work on mobile
    4. simplify footer

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 const loginButton = document.querySelector(".nav-login");
 // signUpRedirectButton()
 const signUpButton = document.querySelector(".nav-register-bubble");
+const startDoingButton = document.querySelector("#registerBtn");
 // toggleHamburgerMenu()
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
@@ -20,6 +21,10 @@ function loginRedirectButton() {
 
 function signUpRedirectButton() {
   signUpButton.addEventListener("click", () => {
+    window.location.href = "signup.html";
+  });
+
+  startDoingButton.addEventListener("click", () => {
     window.location.href = "signup.html";
   });
 }
